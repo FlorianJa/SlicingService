b8d68f08fa689782434363c9106b3c2ac7d76263
AlaaGreatly <alaa.greatly@gmail.com>
2020-12-10 22:07:22 +0100

    - Added check for the requested file name and path - Implemented get method to return all file names

--- SlicerConnector/Controllers/DownloadController.cs
+++ SlicerConnector/Controllers/DownloadController.cs
@@ -10,48 +10,67 @@ using Microsoft.Extensions.Configuration;
 
 namespace SlicerConnector.Controllers
 {
-    
+
     [Route("api/[controller]")]
     [ApiController]
     public class DownloadController : ControllerBase
     {
         private string DataPath;
-
+        // for controlling the connection with Hololense application
         public DownloadController(IConfiguration configuration)
         {
             var tmp = configuration as ConfigurationRoot;
 
             var BasePath = configuration.GetValue<string>("OctoPrint:BasePath");
-            DataPath = Path.Combine(BasePath, "Mehes");
+            DataPath = Path.Combine(BasePath, "Meshes");
+            if (!Directory.Exists(DataPath))
+                Directory.CreateDirectory(DataPath);
         }
 
         // GET: api/<DownloadController>
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            //TODO: list all files
-            return new string[] { "value1", "value2" }; 
+            return System.IO.Directory.GetFiles(DataPath, "*.zip");
         }
 
         [HttpGet("{filename}")]
         public async Task<IActionResult> DownloadFile(string filename)
         {
-            if (filename == null)
-                return Content("filename not present");
-
-            var path = @"D:\SlicerConnector\Meshes\"+filename+".zip";
-
-            //Path.Combine(
-            //               Directory.GetCurrentDirectory(),
-            //               "wwwroot", filename);
-
-            var memory = new MemoryStream();
-            using (var stream = new FileStream(path, FileMode.Open))
+            var filePath = Path.Combine(DataPath, filename + ".zip");
+            if (CheckFileAvailability(filename, filePath, out string message))
             {
-                await stream.CopyToAsync(memory);
+                var memory = new MemoryStream();
+                using (var stream = new FileStream(filePath, FileMode.Open))
+                {
+                    await stream.CopyToAsync(memory);
+                }
+                memory.Position = 0;
+                return File(memory, "application/zip", Path.GetFileName(filePath));
             }
-            memory.Position = 0;
-            return File(memory, "application/zip", Path.GetFileName(path));
+
+            else
+            {
+                return StatusCode(404, message);
+            }
+        }
+
+        private bool CheckFileAvailability(string filename, string filepath, out string message)
+        {
+            message = "";
+            if (String.IsNullOrWhiteSpace(filename))
+            {
+                message = "filename not present";
+                return false;
+            }
+
+            if (!System.IO.File.Exists(filepath))
+            {
+                message = "The requested file was not found";
+                return false;
+            }
+
+            return true;
         }
     }
 }
